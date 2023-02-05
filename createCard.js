5c623948eabb9fb66b115573014b36abb53922ac
tuckie <22203973+twuky@users.noreply.github.com>
2023-02-05 00:11:38 -0800

    cards draw artwork

--- createCard.js
+++ createCard.js
@@ -4,6 +4,8 @@ export const createCard = (data, status) => {
     //status: deck | hand | inPlay | discard
     //kind: grifts | frauds | propups | specials
 
+    loadSprite("cardBackground", "./assets/sprites/cards/CardBase.png")
+
     const newCard = add([
         data,   
         "card",
@@ -15,12 +17,31 @@ export const createCard = (data, status) => {
     ])
 
     newCard.add([
+        "cardBackground",
+        sprite("cardBackground"),
+        scale(4),
+        pos(0,-10)
+    ])
+
+    if (typeof data.artwork === "string") {
+        
+        loadSprite(`cardArt_${data.artwork}`, `./assets/sprites/cards/${data.artwork}.png`)
+
+        newCard.add([
+            "cardArt",
+            sprite(`cardArt_${data.artwork}`),
+            scale(4),
+            pos(0,9)
+        ])
+    }
+
+    newCard.add([
         "cardName",
         color(black),
         text(data.name, {
             size: 24
         }),
-        pos(0, -40)
+        pos(6, 200)
     ])
 
     if(newCard.is("grifts")) {
